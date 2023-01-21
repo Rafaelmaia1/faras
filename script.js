b03073986e2db760b0039e9539d567f7b05e841c
Rafael Maia <maia.27rafael@mail.com>
2023-01-21 07:35:48 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form')
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+    const dayExists =nlwSetup.dayExists(today)
+
+    if(dayExists) {
+    alert("Dia já incluso.")
+    return
+    }
+alert("dia adicionado ✅.")
+    nlwSetup.addDay(today)
+}
+
+
+function save(){
+    localStorage.setItem('NLW', JSON.stringify(nlwSetup.data))
+   
+}
+
+//const data ={
+    //run:["01-20", "02-01", "03-01", "04-01", "05-01", "06-01", "07-01","08-01", "09-01", "10-01", "11-01", "12-01", "13-01", "14-01"],
+    //takepills: ['02-01'],
+    //Journal:["02-01"],
+//}
+
+const data = JSON.parse(localStorage.getItem("NLW"))
+nlwSetup.setData(data)
+ nlwSetup.load()
